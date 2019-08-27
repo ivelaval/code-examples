371bbf40d22c9d935dc52ac0c9b7bfaff8afba4c
Ivan Avila <ivelaval@gmail.com>
2019-08-26 22:16:47 -0500

    Several examples related to closures were added

---
+++ javascript/fibonacci-with-closures.js
@@ -0,0 +1,16 @@
+var contador = 0;
+
+var fibonacci = function (n) {
+  // Counter for seeing how many times the function is executed 
+  contador++;
+  // we receive a number if it is less than two we return the number,
+  // else, to do two recursive callings to the function
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+// Loop where we go passing number one by one to the fibonacci function and showing the result
+for (var i = 0; i <= 10; i += 1) {
+  console.log(i + ': ' + fibonacci(i));
+}
+
+console.log(contador); //453
